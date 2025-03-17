2411166c7c5821b2a01f375aa509a6719d0fe958
dibarra3 <160536459+dibarra3@users.noreply.github.com>
2025-03-17 17:31:12 -0500

    Added app des, images, links

--- main.py
+++ main.py
@@ -22,12 +22,18 @@ st.write("Below you can find some apps I have built in Python.\n\n"
 
 df = pandas.read_csv("data.csv", sep=";")
 
-col3, col4 = st.columns(2)
+col3, emptyCol, col4 = st.columns([1.5,0.5,1.5])
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
